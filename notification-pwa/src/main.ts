b919d6cfcd55e39970d1f441b0e298de8fb3fc92
Daniel B <danielb.alzate1@gmail.com>
2020-01-24 19:04:15 -0500

    Primer versión en Español

--- notification-pwa/src/main.ts
+++ notification-pwa/src/main.ts
@@ -1,13 +1,32 @@
-import Vue from 'vue';
-import App from './App.vue';
-import './registerServiceWorker';
-import router from './router';
-import store from './store';
+import * as firebase from "firebase";
 
-Vue.config.productionTip = false;
+var config = {
+	apiKey: "AIzaSyBT1ovsd4nEBJkOuMpBQDw490qOL5aQQJw",
+	authDomain: "my-pwa-notification.firebaseapp.com",
+	databaseURL: "https://my-pwa-notification.firebaseio.com",
+	projectId: "my-pwa-notification",
+	storageBucket: "my-pwa-notification.appspot.com",
+	messagingSenderId: "821779390602",
+	appId: "1:821779390602:web:3eac6c6511179996298d73",
+	measurementId: "G-15QXEHXMDR"
+}; // 4. Get Firebase Configuration
+firebase.initializeApp(config);
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+const messaging = firebase.messaging();
+
+messaging.usePublicVapidKey("BJHrWSsMWtIPsBAyVfWeEvIy6uWslPMWSHoSx9vzOb2oB3eRslwWioboFcbs61hHNpvyl91GCfNEZ_SYpbwYfrc"); // 1. Generate a new key pair
+
+// Request Permission of Notifications
+messaging
+	.requestPermission()
+	.then(() => {
+		console.log("Notification permission granted.");
+
+		// Get Token
+		messaging.getToken().then((token) => {
+			console.log(token);
+		});
+	})
+	.catch((err) => {
+		console.log("Unable to get permission to notify.", err);
+	});
