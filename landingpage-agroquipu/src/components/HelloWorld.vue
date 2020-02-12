d0dd4c7dbb8d75eda60521993ac7b90cadbd9851
Daniel B <danielb.alzate1@gmail.com>
2020-02-12 17:19:19 -0500

    Ajuste gráfico formulario contacto

--- landingpage-agroquipu/src/components/HelloWorld.vue
+++ landingpage-agroquipu/src/components/HelloWorld.vue
@@ -5,10 +5,19 @@
 				<v-parallax src="../assets/agro.jpg" height="600" style="">
 					<v-layout column align-center justify-center class="white--text">
 						<!-- <img src="../assets/brote.png" class="mb-5" height="200" /> -->
+						<div class="" style="background-color: #ccc; opacity:0.6; width: 100%; height: 50%; border-radius: 25px; position:absolute"></div>
+						<v-card flat class="my-12 mx-12 pa-1" color="rgb(186, 201, 38)">
+							<v-card-title primary-title class="layout justify-center">
+								<div class="display-3 font-weight-bold" style="color:white">¡Bienvenido AgroQuipu!</div>
+							</v-card-title>
+							<v-card-text class="text-center font-weight-light display-1" style="color:white">
+								Donde cuidar el planeta es crear oportunidades
+							</v-card-text>
+						</v-card>
 					</v-layout>
 				</v-parallax>
 			</section>
-			<section>
+			<!-- <section>
 				<v-layout column wrap class="my-12" align-center>
 					<v-card flat class="my-12 mx-12">
 						<v-card-title primary-title class="layout justify-center">
@@ -22,7 +31,7 @@
 			</section>
 			<section>
 				<v-divider></v-divider>
-			</section>
+			</section> -->
 			<section>
 				<v-layout column wrap class="my-12" align-center>
 					<v-col lg="10">
@@ -46,7 +55,7 @@
 				<v-layout column wrap class="my-12" align-center>
 					<v-flex xs12 sm4 class="my-4">
 						<div class="text-center">
-							<h2 class="display-2">Testimonios</h2>
+							<h2 class="display-3 font-weight-bold">Testimonios</h2>
 						</div>
 					</v-flex>
 					<v-flex xs12>
@@ -99,6 +108,9 @@
 				</v-layout>
 			</section>
 			<section>
+				<v-divider></v-divider>
+			</section>
+			<!-- 			<section>
 				<v-layout column wrap class="my-12" align-center>
 					<v-card flat class="my-12 mx-12">
 						<v-card-title primary-title class="layout justify-center">
@@ -109,8 +121,16 @@
 						</v-card-text>
 					</v-card>
 				</v-layout>
-			</section>
+			</section> -->
 			<section>
+				<v-card flat class="my-12 mx-12">
+					<v-card-title primary-title class="layout justify-center">
+						<div class="display-3 font-weight-bold">Nuestros servicios AgroQuipu</div>
+					</v-card-title>
+					<v-card-text class="text-center font-weight-light display-1">
+						Estamos aquí para crecer juntos
+					</v-card-text>
+				</v-card>
 				<div class="d-flex">
 					<v-col lg="4">
 						<v-card-title primary-title class="layout justify-center">
@@ -138,42 +158,32 @@
 					</v-col>
 				</div>
 			</section>
-
+			<section>
+				<v-divider></v-divider>
+			</section>
 			<section>
 				<v-container grid-list-xl>
 					<v-layout row wrap justify-center class="my-12">
 						<v-flex xs12 sm4>
 							<v-card flat class="transparent">
 								<v-card-title primary-title class="layout justify-center">
-									<div class="headline">Company info</div>
-								</v-card-title>
-								<v-card-text>
-									Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
-									turpis egestas. Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
-								</v-card-text>
-							</v-card>
-						</v-flex>
-
-						<v-flex xs12 sm4 offset-sm1>
-							<v-card flat class="transparent">
-								<v-card-title primary-title class="layout justify-center">
 									<div class="headline">Contáctenos</div>
 								</v-card-title>
-								<v-card-text>
+								<v-card-text class="title font-weight-light">
 									Sé parte de nuestra comunidad
 								</v-card-text>
 								<v-list class="transparent">
 									<v-list-item>
 										<v-list-item-action>
-											<v-icon class="blue--text text--lighten-2">mdi-phone</v-icon>
+											<v-icon class="blue--text text--blue darken-2">mdi-face</v-icon>
 										</v-list-item-action>
 										<v-list-item-content>
-											<v-list-item-title><v-text-field v-model="lastname" :rules="nameRules" :counter="30" label="Nombre completo" required></v-text-field></v-list-item-title>
+											<v-list-item-title><v-text-field v-model="name" :rules="nameRules" :counter="30" label="Nombre completo" required></v-text-field></v-list-item-title>
 										</v-list-item-content>
 									</v-list-item>
 									<v-list-item>
 										<v-list-item-action>
-											<v-icon class="blue--text text--lighten-2">mdi-map-marker</v-icon>
+											<v-icon class="blue--text text--blue darken-2">mdi-email</v-icon>
 										</v-list-item-action>
 										<v-list-item-content>
 											<v-list-item-title><v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field></v-list-item-title>
@@ -181,13 +191,35 @@
 									</v-list-item>
 									<v-list-item>
 										<v-list-item-action>
-											<v-icon class="blue--text text--lighten-2">mdi-email</v-icon>
+											<v-icon class="blue--text text--blue darken-2">mdi-phone</v-icon>
 										</v-list-item-action>
 										<v-list-item-content>
-											<v-list-item-title><v-text-field v-model="lastname" :rules="nameRules" :counter="10" label="Last name" required></v-text-field></v-list-item-title>
+											<v-list-item-title>
+												<v-text-field type="number" v-model="phone" :rules="phoneRules" :counter="10" label="Teléfono o celular" required></v-text-field>
+											</v-list-item-title>
 										</v-list-item-content>
 									</v-list-item>
+									<v-card-title primary-title class="layout justify-center">
+										<v-btn outlined color="green" class="mr-4" @click="submit">Enviar</v-btn>
+										<v-btn outlined color="blue darken-2" @click="clear">Limpiar fomulario</v-btn>
+									</v-card-title>
 								</v-list>
+							</v-card>
+						</v-flex>
+						<v-flex xs12 sm4>
+							<v-card-title primary-title class="layout justify-center">
+								<div class="headline">¡Selecciona tus intereses!</div>
+							</v-card-title>
+							<v-col cols="12">
+								<v-select v-model="value" :items="items" chips label="Quiero hacer parte de..." multiple outlined></v-select>
+							</v-col>
+							<v-card flat class="transparent">
+								<v-card-title primary-title class="layout justify-center">
+									<div class="headline">¡Queremos escucharte!</div>
+								</v-card-title>
+								<v-card-text>
+									<v-textarea outlined name="input-7-4" label="¡Esperamos tus comentarios!" value=""></v-textarea>
+								</v-card-text>
 							</v-card>
 						</v-flex>
 					</v-layout>
@@ -212,11 +244,24 @@
 export default {
 	data: () => ({
 		valid: false,
-		firstname: "",
-		lastname: "",
+		name: "",
+		phone: "",
 		nameRules: [(v) => !!v || "Para continuar debe ingresar su nombre", (v) => v.length <= 30 || "No debe superar las 30 letras"],
+		phoneRules: [(v) => !!v || "Para continuar debe ingresar su teléfono", (v) => v.length <= 10 || "No debe superar las 10 letras"],
 		email: "",
-		emailRules: [(v) => !!v || "Para continuar debe ingresar su e-mail", (v) => /.+@.+/.test(v) || "Por favor verifique su e-mail"]
-	})
+		emailRules: [(v) => !!v || "Para continuar debe ingresar su e-mail", (v) => /.+@.+/.test(v) || "Por favor verifique su e-mail"],
+		items: ["Nuestros aprendices en agroecología", "Compradores de productos agroecológicos", "Vendedores de productos agroecológicos"],
+		value: ["Nuestros aprendices en agroecología", "Compradores de productos agroecológicos", "Vendedores de productos agroecológicos"]
+	}),
+	methods: {
+		submit() {},
+		clear() {
+			this.name = "";
+			this.email = "";
+			this.phone = "";
+			this.items = ["Nuestros aprendices en agroecología"];
+			this.value = ["Nuestros aprendices en agroecología"];
+		}
+	}
 };
 </script>
