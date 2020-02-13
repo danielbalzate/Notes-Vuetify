9d674f3437732f0edd552f2d3ca52a167cba25c0
Daniel B <danielb.alzate1@gmail.com>
2020-02-12 23:24:09 -0500

    Implementacion Storage con Firebase

--- landingpage-agroquipu/src/main.js
+++ landingpage-agroquipu/src/main.js
@@ -1,14 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify';
-
-Vue.config.productionTip = false
+/* Vue */
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+/* Vuetify */
+import vuetify from "./plugins/vuetify";
+// Vue
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	store,
+	vuetify,
+	render: (h) => h(App)
+}).$mount("#app");
