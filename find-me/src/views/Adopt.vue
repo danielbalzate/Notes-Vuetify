bb4d4b7075639bc52f2e35a4843723f62b27954d
Daniel B <danielb.alzate1@gmail.com>
2020-02-24 23:29:54 -0500

    Ajustes formulario adopta

--- find-me/src/views/Adopt.vue
+++ find-me/src/views/Adopt.vue
@@ -1,12 +1,12 @@
 <template>
-	<v-card class="d-flex flex-wrap" flat tile>
-		<v-card max-width="250" class="ma-3">
+	<v-card class="d-flex flex-wrap" flat tile no-gutters>
+		<v-card max-width="250" height="100%" class="ma-1">
 			<v-list-item>
 				<v-avatar class="mr-5">
 					<img :src="user.photo" />
 				</v-avatar>
 				<v-list-item-content>
-					<v-list-item-title class="headline">ESTO ES UN MENSAJE ESTATICO PAI</v-list-item-title>
+					<v-list-item-title class="headline">¡Bienvenido!</v-list-item-title>
 					<v-list-item-subtitle>{{ user.name }}</v-list-item-subtitle>
 				</v-list-item-content>
 			</v-list-item>
@@ -30,9 +30,23 @@
 													</v-card-title>
 
 													<v-card-text class="title font-weight-light">
-														<v-text-field v-model="titlePost" :rules="titlePostRules" :counter="30" label="Título del post" required></v-text-field>
+														<v-text-field v-model="titlePost" :rules="titlePostRules" :counter="10" label="Título del post" required></v-text-field>
 													</v-card-text>
-
+													<v-card-text>
+														<input type="file" ref="btnUploadFile" class="d-none" @change="searchImg($event)" />
+														<v-btn @click="$refs.btnUploadFile.click()" color="blue" class="white--text ma-1">
+															<v-icon dark right class="mr-3">fas fa-person-booth</v-icon>
+															Subir imagen
+														</v-btn>
+														<v-card-text v-if="file">
+															<h4>Nombre del archivo: {{ file.name }}</h4>
+															<v-img width="15%" :src="urlTmp"></v-img>
+														</v-card-text>
+														<!-- <v-btn :disabled="file === null" @click="uploadImg()" color="lime" class="white--text ma-1">
+															<v-icon dark right class="mr-3">fas fa-rocket</v-icon>
+															Subir
+														</v-btn> -->
+													</v-card-text>
 													<v-list class="transparent">
 														<v-list-item>
 															<v-textarea outlined v-model="messagePost" name="input-7-4" label="¡Esperamos tus mensajes!" value=""></v-textarea>
@@ -53,19 +67,23 @@
 				</v-row>
 			</v-card-text>
 		</v-card>
-		<v-card max-width="250" class="ma-3" v-for="(post, index) in posts" :key="index">
+
+		<v-card max-width="250" height="100%" class="ma-1" v-for="(post, index) in posts" :key="index">
 			<v-list-item>
 				<v-avatar class="mr-5">
-					<img :src="post.avatarUserPost" />
+					<img :src="user.photo" />
 				</v-avatar>
 				<v-list-item-content>
 					<v-list-item-title class="headline">{{ post.titlePost }}</v-list-item-title>
-					<v-list-item-subtitle>{{ post.userPost }}</v-list-item-subtitle>
+					<v-list-item-subtitle>{{ user.name }}</v-list-item-subtitle>
 				</v-list-item-content>
 			</v-list-item>
 
 			<v-img :src="post.imgPost" height="194"></v-img>
-
+			<v-chip class="ma-2" color="indigo darken-3" outlined>
+				<v-icon left>mdi-fire</v-icon>
+				{{ post.registerDate }}
+			</v-chip>
 			<v-card-text>
 				{{ post.messagePost }}
 			</v-card-text>
@@ -74,17 +92,20 @@
 </template>
 <script>
 import {mapState} from "vuex";
-import {db} from "@/firebase";
+import {firebase, storage, db} from "@/firebase";
 import Swal from "sweetalert2";
 
 // @ is an alias to /src
 export default {
 	data() {
 		return {
-			valid: false,
+			/* Upload image */
+			file: null,
+			urlTmp: "",
 			/* Rules form */
+			valid: false,
 			isValid: true,
-			titlePostRules: [(v) => !!v || "Para continuar debe ingresar un título", (v) => v.length <= 30 || "No debe superar las 30 letras"],
+			titlePostRules: [(v) => !!v || "Para continuar debe ingresar un título", (v) => v.length <= 10 || "No debe superar las 10 letras"],
 			titlePost: "",
 			messagePost: null,
 			dialog: false,
@@ -104,44 +125,70 @@ export default {
 		// Creo que no lo voy a utilizar modular
 		// ...mapState("posts", ["posts"])
 	},
-	created() {},
-	mounted() {
-		this.getChatGlobal();
+	created() {
+		this.getPostGlobal();
 	},
+	mounted() {},
 	methods: {
 		clear() {
 			this.titlePost = "";
 			this.messagePost = "";
 		},
-		async getChatGlobal() {
+		async getPostGlobal() {
 			try {
-				const resDB = await db.collection("chatGlobal").get();
+				const resDB = await db
+					.collection("users")
+					.doc(this.user.uid)
+					.collection("posts")
+					.get();
 				resDB.forEach((res) => {
-					// console.log("TCL: getChatGlobal -> res", res);
-					const chatGlobal = {
+					// console.log("TCL: getPostGlobal -> res", res);
+					const postGlobal = {
 						avatarUserPost: res.data().avatarUserPost,
 						userPost: res.data().userPost,
 						titlePost: res.data().titlePost,
 						imgPost: res.data().imgPost,
-						messagePost: res.data().messagePost
+						messagePost: res.data().messagePost,
+						registerDate: res.data().registerDate
 					};
-					this.posts.push(chatGlobal);
+					this.posts.push(postGlobal);
 				});
 			} catch (error) {
-				console.log("TCL: getChatGlobal -> error", error);
+				console.log("TCL: getPostGlobal -> error", error);
 			}
 		},
 		async addPost() {
 			try {
-				if (this.titlePost && this.messagePost) {
-					const resDB = await db.collection("chatGlobal").add({
-						avatarUserPost: this.user.photo,
-						userPost: this.user.name,
-						userUID: this.user.uid,
-						titlePost: this.titlePost,
-						messagePost: this.messagePost,
-						imgPost: "https://cdn.vuetifyjs.com/images/cards/mountain.jpg"
+				if (this.titlePost && this.messagePost && this.file) {
+					/* Todo esto hace parte a la imagen */
+					Swal.fire({
+						title: "¡Estamos retocando la foto de tu peludo!",
+						onBeforeOpen: () => {
+							Swal.showLoading();
+						}
 					});
+					const actualDate = Date.now().toString();
+					// console.log("TCL: addPost -> actualDate", actualDate);
+					const refImg = storage
+						.ref()
+						.child("Post")
+						.child(actualDate);
+					const res = await refImg.put(this.file);
+					// console.log(res);
+					const urlDownload = await refImg.getDownloadURL();
+					/* Sube la info */
+					const resDB = await db
+						.collection("users")
+						.doc(this.user.uid)
+						.collection("posts")
+						.add({
+							avatarUserPost: this.user.photo,
+							userPost: this.user.name,
+							titlePost: this.titlePost,
+							messagePost: this.messagePost,
+							imgPost: urlDownload,
+							registerDate: firebase.firestore.Timestamp.fromDate(new Date())
+						});
 					Swal.fire({
 						icon: "success",
 						title: "¡Has posteado correctamente!",
@@ -150,9 +197,10 @@ export default {
 					});
 					(this.posts = []), (this.dialog = false);
 					this.titlePost = "";
+					this.file = null;
 					// Creo que esto podría mejorarse con un Snapshot
 					this.messagePost = "";
-					this.getChatGlobal();
+					this.getPostGlobal();
 				} else {
 					Swal.fire({
 						icon: "error",
@@ -164,6 +212,30 @@ export default {
 			} catch (error) {
 				console.log("TCL: addPost -> error", error);
 			}
+		},
+		searchImg(event) {
+			const typeFile = event.target.files[0].type;
+
+			if (typeFile === "image/jpeg" || typeFile === "image/png") {
+				// console.log(event.target.files[0]);
+				this.file = event.target.files[0];
+			} else {
+				Swal.fire({
+					icon: "error",
+					title: "Oops... Archivo no válido",
+					text: "¡Debes seleccionar una imagen!"
+				});
+				this.error = "Archivo no valido";
+				this.file = null;
+				return;
+			}
+
+			const reader = new FileReader();
+			reader.readAsDataURL(this.file);
+			reader.onload = (e) => {
+				// console.log(e.target.result);
+				this.urlTmp = e.target.result;
+			};
 		}
 	}
 };
